e0c64638651dad6b9171faf8a933f5740f8030a7
Joanna Krol <joannakrol@Rustams-MacBook-Air.local>
2020-06-03 13:31:03 +0200

    Added logout in the reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -46,6 +46,14 @@ const authFail = (state, action) => {
     } 
     );
 }
+
+//logout:
+const authLogout = (state, action) => {
+    return updatedObject(state, {
+        token: null,
+        userId: null
+    })
+}
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SIGNIN_START: return authStartSignIn(state, action);
@@ -54,6 +62,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         default: return state;    
     }
 }
